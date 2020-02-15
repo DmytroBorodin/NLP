f554135e8907d5ecfa775d0e944f22776cec684b
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-02-15 23:20:25 +0200

    Add files via upload

---
+++ js/nlp.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.floor-slider').slick({
+    infinite: true,
+	dots: true,
+	nextArrow: '<span class="icon-right r-arrow arrow"></span>',
+	prevArrow: '<span class="icon-left l-arrow arrow"></span>',
+  });
+});
